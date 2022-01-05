c20af6ad7f4297fa3dcbbf892ad3a44c5f06de33
kfpopeye <kfpopeye@users.noreply.github.com>
2022-01-05 13:12:57 -0800

    Update light.lua
    Corrected light on\off

--- Lua/light.lua
+++ Lua/light.lua
@@ -2,11 +2,11 @@ battery = component.proxy("3909A0A2414FAE74945E39B4BA310CF0")
 light = component.proxy("ED45EBA041045EB6E9909A8E10FA4904")
 while true do
   if (battery.powerStorePercent <= 0.75) then
-   light.isLightEnabled = true
-   print(computer.millis(), battery.powerStorePercent * 100, "%", "Light on !!!!!!!!!!!!!!")
-  else
+   print(computer.millis(), battery.powerStorePercent * 100, "%", "Light off !!!!!!!!!!!!!!")
    light.isLightEnabled = false
-   print(computer.millis(), battery.powerStorePercent * 100, "%", "Light off")
+  else
+   light.isLightEnabled = true
+   print(computer.millis(), battery.powerStorePercent * 100, "%", "Light on")
   end
 
   event.pull(10.0)
