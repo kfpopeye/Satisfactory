698ac42e9dc22bc6638a121fd74bf3609eb12380
kfpopeye <kfpopeye@users.noreply.github.com>
2022-01-21 16:25:45 -0800

    Update light.lua
    Changed information to read from power circuit instead of battery

--- Lua/light.lua
+++ Lua/light.lua
@@ -1,14 +1,17 @@
 battery = component.proxy("3909A0A2414FAE74945E39B4BA310CF0")
 light = component.proxy("ED45EBA041045EB6E9909A8E10FA4904")
+
+circuit = battery:getPowerConnectors()[1]:getCircuit()
+
 while true do
-  if (battery.powerStorePercent <= 0.75) then
+  if (circuit.batteryStorePercent <= 0.75) then
    light.colorSlot = 1
-  elseif (battery.powerStorePercent < 1.0) then
+  elseif (circuit.batteryStorePercent < 1.0) then
    light.colorSlot = 2
   else
    light.colorSlot = 3
   end
 
-  print(computer.millis(), battery.powerStorePercent * 100, "%")
+  print(computer.millis(), circuit.batteryStorePercent * 100, "%")
   event.pull(10.0)
 end
