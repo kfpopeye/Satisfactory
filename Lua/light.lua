d7a6b2e888b4ed23de12d90a47aaabe390b34a7b
kfpopeye <kfpopeye@users.noreply.github.com>
2022-01-21 16:11:19 -0800

    Update light.lua
    Updated light colors

--- Lua/light.lua
+++ Lua/light.lua
@@ -2,12 +2,13 @@ battery = component.proxy("3909A0A2414FAE74945E39B4BA310CF0")
 light = component.proxy("ED45EBA041045EB6E9909A8E10FA4904")
 while true do
   if (battery.powerStorePercent <= 0.75) then
-   print(computer.millis(), battery.powerStorePercent * 100, "%", "Light off !!!!!!!!!!!!!!")
-   light.isLightEnabled = false
+   light.colorSlot = 1
+  elseif (battery.powerStorePercent < 1.0) then
+   light.colorSlot = 2
   else
-   light.isLightEnabled = true
-   print(computer.millis(), battery.powerStorePercent * 100, "%", "Light on")
+   light.colorSlot = 3
   end
 
+  print(computer.millis(), battery.powerStorePercent * 100, "%")
   event.pull(10.0)
 end
