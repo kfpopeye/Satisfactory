a9291dc33326e632d9222517d4dcb6a1791c48a7
kfpopeye <kfpopeye@users.noreply.github.com>
2022-03-01 17:18:53 -0800

    Update inventory_merger.lua

--- Lua/inventory_merger.lua
+++ Lua/inventory_merger.lua
@@ -17,37 +17,83 @@ function tableLength(T)
   return count
 end
 
-function updateInfo() --120 x 30
+function updateInfo()
  if not hasScreen then return end
  local row = 14
  local col = 0
+ local height = 17
+ local width = 120
+
+ gpu:setBackground(0,0,0,0)
+ gpu:setForeground(1,1,1,1)
+ gpu:fill(col, row, width, height, " ")
+
+ gpu:setBackground(1,1,1,1)
+ gpu:setForeground(0,0,0,1)
+ gpu:setText(col, row, "Progress" .. string.rep(" ", 112))
+ row = row + 1
  
  gpu:setBackground(0,0,0,0)
  gpu:setForeground(1,1,1,1)
- gpu:fill(0, row, 30, 1, " ")
  gpu:setText(col, row, "Items transitting: " .. itemsInTransit)
+ if (itemsInTransit == 0) then 
+  gpu:setForeground(0,1,0,1)
+  gpu:setText(col + 25, row, "**** COMPLETE ****")
+  gpu:setForeground(1,1,1,1)
+ end
+ row = row + 1
+ gpu:setText(col, row, "Codeable mergers found: " .. mergerCount)
+ row = row + 2
+ 
+ gpu:setBackground(1,1,1,1)
+ gpu:setForeground(0,0,0,1)
+ gpu:setText(col, row, "Items filling")
+ row = row + 1
+
+ gpu:setBackground(0,0,0,0)
+ gpu:setForeground(1,1,1,1)
+ local rowStart = row
+ for _, infoTable in pairs(slotsToRefill) do
+  local n = string.sub((infoTable["name"] .. "          "), 1, 10) 
+  gpu:setText(col, row, n .. " (" .. infoTable["count"] .. "\\" .. infoTable["max"] .. ")")
+  row = row + 1
+  if (row == rowStart + 12) then
+   row = rowStart
+   col = col + 30
+  end
+ end
  gpu:flush()
 end
 
-function printInventoryToScreen(msg) -- resolution 120x30
+-- passing a string will add it to the buffer, passing nil will display buffer to screen
+function printInventoryToScreen(msg)
  if not hasScreen then return end
  local row = 0
  local col = 0
+ local height = 13
+ local width = 120
  if not inventoryBuffer then inventoryBuffer = {} end
  if (msg) then
   table.insert(inventoryBuffer, msg)
  else
   gpu:setBackground(0,0,0,0)
-  gpu:fill(0, row, 120, row + 12, " ")
+  gpu:fill(col, row, width, height, " ")
   gpu:flush()
+
+  gpu:setBackground(1,1,1,1)
+  gpu:setForeground(0,0,0,1)
+  gpu:setText(col, row, "Catalogued container inventory".. string.rep(" ", 90))
+
   gpu:setBackground(0,0,0,0)
   gpu:setForeground(1,1,1,1)
+  row = row + 1
+  local rowStart = row
   for k,v in pairs(inventoryBuffer) do
    gpu:setText(col, row, inventoryBuffer[k])
    inventoryBuffer[k]=nil
    row = row + 1
-   if (row == 12) then
-    row = 0
+   if (row == rowStart + 12) then
+    row = rowStart
     col = col + 30
    end
   end
@@ -83,7 +129,7 @@ function createInventoryList()
     end
    end
    print(" Slot", i, name, c, m)
-   printInventoryToScreen("Slot #" .. i .. ": " .. name .. "(" .. c .. "/" .. m .. ")")
+   printInventoryToScreen("Slot #" .. i .. ": " .. string.sub((name .. "          "), 1, 10) .. " (" .. c .. "\\" .. m .. ")")
   else
    printInventoryToScreen("Slot #" .. i .. ": Empty")
   end
@@ -114,6 +160,7 @@ function getMergers()
   mergers = component.proxy(component.findComponent(groupName))
  end
 
+ mergerCount = tableLength(mergers)
  return mergers
 end
 
@@ -122,8 +169,7 @@ function findMerger(intName)
  local returnMerger = nil
  local returnInput = -1
  local mergers = getMergers()
- local count = tableLength(mergers)
- if count == 0 then error("No Codeable Mergers found!") else print(count, "mergers found.") end
+ if count == 0 then error("No Codeable Mergers found!") else print("Findmerger() found " .. mergerCount .. " mergers.") end
 
  for _, merger in pairs(mergers) do
   local x = 0
@@ -155,28 +201,25 @@ function checkEvents()
  e, sender = event.pull(0)
  if (e == "ItemTransfer") then
   itemsInTransit = itemsInTransit - 1
-  updateInfo()
  end
 end
 
 function processPassthruInputs()
  local dbgMsg = nil
  local mergers = getMergers()
- local count = tableLength(mergers)
- if count == 0 then error("No Codeable Mergers found!") else print("Passthru() found " .. count .. " mergers.") end
+ if count == 0 then error("No Codeable Mergers found!") else print("processPassthruInputs() found " .. mergerCount .. " mergers.") end
 
  for _, merger in pairs(mergers) do
   local x = 0
   while (x < 3) do
    local i = merger:getInput(x)
    if (i and i.type) then
-    dbgMsg = merger.nick
     if isPassThrough(merger, x) then
      if merger.canOutput then
-      dbgMsg = dbgMsg .. " - passthru " .. i.type.name .. " on input " .. x
+      dbgMsg = " " .. merger.nick .. " - passed thru " .. i.type.name .. " on input " .. x
       merger:transferItem(x)
      else
-      dbgMsg = dbgMsg .. " - cannot output " .. i.type.name
+      dbgMsg = " " .. merger.nick .. " - cannot output " .. i.type.name
      end
     end
    end
@@ -190,6 +233,9 @@ end
 -- ***************** constants ********************
 direction = {left = 2, middle = 1, right = 0}
 
+-- ***************** globals ********************
+mergerCount = 0
+
 -- ***************** devices ********************
 hasScreen = false
 local screen = computer.getPCIDevices(findClass("FINComputerScreen"))[1]
@@ -198,6 +244,7 @@ gpu = computer.getPCIDevices(findClass("GPUT1"))[1]
 if not gpu then print("No GPU found. Did you add a graphical processing unit T1?") end
 if screen and gpu then
  gpu:bindScreen(screen)
+ gpu:setSize(120, 50)
  hasScreen = true
  clearTabScreen()
 end
@@ -221,6 +268,7 @@ end
 itemsInTransit = 0
 while needsRefill do
  needsRefill = false
+
  for intName, infoTable in pairs(slotsToRefill) do
   local dbgMsg = nil
   if(intName) then
@@ -243,14 +291,18 @@ while needsRefill do
    end
   end
   processPassthruInputs()
+  updateInfo()
   checkEvents()
  end --endfor
+
 end
 
 while itemsInTransit > 0 do
  processPassthruInputs()
+ updateInfo()
  checkEvents()
 end
 
+updateInfo()
 computer.beep()
 print ("Done")
