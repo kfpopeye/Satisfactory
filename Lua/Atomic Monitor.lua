0e88b531e4f30d37b374453c5efb29f49b4ef051
kfpopeye <kfpopeye@users.noreply.github.com>
2024-11-25 20:23:21 -0800

    Update Atomic Monitor.lua
    undo last commit

--- Lua/Atomic Monitor.lua
+++ Lua/Atomic Monitor.lua
@@ -1,9 +1,30 @@
 -- -------------------------------------------------------------
 -- |                                                           |
--- |   Hub atomic report.lua                                   |
+-- |   Atomic Monitor.lua                                      |
 -- |                                                           |
 -- -------------------------------------------------------------
-computer.log(1, "--- Atomic Atomic Report v1.0 ---")
+computer.log(1, "--- Atomic Monitor v1.0 ---")
+
+-- IF the string is more than 1 word (contains spaces) and greater than 5 characters
+-- this will remove all lowercase letters, remove all dashes and convert spaces to periods
+function createAbbrev(s)
+ if (string.find(s, "%s")) then
+  if (s:len() > 5) then
+   return string.gsub(string.gsub(string.gsub(s, "%l", ""), "%s", "."), "%-", "")
+  end
+ end
+ return s
+end
+
+function round(n)
+ return n % 1 >= 0.5 and math.ceil(n) or math.floor(n)
+end
+
+function makePercentage(x)
+ local num = x * 100
+ num = round (num)
+ return num
+end
 
 function clearScreen(g)
  local w,h = g:getSize()
@@ -34,213 +55,231 @@ function convertToTime(ms)
  return string.format("%02d:%02d:%02d.%01d", hh, mm, ss, t)
 end
 
-function updateData(data, ctable, ftable, rtable)
- local d1, d2, d3, d4, d5, d6, d7 = table.unpack(data)
-  if d1 == "container" then
-   ctable[d2] = d3
-  elseif d1 == "factory" then
-   local fdata = {}
-   fdata["outputs"] = d3
-   fdata["inputs"] = d4
-   if(d5) then fdata["productivity"] = d5 else fdata["productivity"] = "--" end   
-   ftable[d2] = fdata
-  elseif d1 == "reactor" then
-   local fdata = {}
-   fdata["outputs"] = d3
-   fdata["inputs"] = d4
-   if(d5) then fdata["productivity"] = d5 else fdata["productivity"] = "--" end   
-   rtable[d2] = fdata
+function displayContainers()
+ local containers = component.proxy(component.findComponent("STORAGE"))
+ if not containers then error("Containers was nil") end
+ print("Number of containers: " .. tableLength(containers))
+ if (tableLength(containers) == 0) then return end
+
+ gpu:setBackground(0, 0.5, 1.0, 0.5)
+ gpu:setForeground(0, 0, 0, 1)
+ gpu:setText(0, row, string.format("%-64s", "Container Inventory")) --65 chars (padded with spaces after)
+ gpu:setBackground(0,0,0,0)
+ gpu:setForeground(1,1,1,1)
+
+ row = row + 1
+ local topRow = row
+ local col = 1
+ for _, cntr in pairs(containers) do
+  --print ("Type: " .. cntr:getType().displayName)
+  local invs = cntr:getInventories()[1]
+  local count = 0
+  local max = -1
+  local name = cntr.nick:sub(9)
+  local i = 0
+  
+  if (invs) then
+   while (i < invs.Size) do
+    local t = nil
+    local stack = invs:getStack(i)
+    if (stack.item) then t = stack.item.type end
+    if(t) then
+     count = count + stack.count
+     max = t.max * invs.Size
+    end
+    i = i + 1
+   end --while
+  else
+   count = cntr.fluidContent
+   max = cntr.maxFluidContent
+  end --if
+  
+  local _, h = gpu:getSize()
+  if (row > h-2 and col == 1) then
+   col = 30
+   row = topRow
   end
+
+  local percentage = makePercentage(count / max)
+  gpu:setText(col, row, string.format("%-20s", name) .. string.format("%3s", percentage) .. "%")
+  net:broadcast(port, "container", name, percentage)
+  row = row + 1
+ end --for
 end
 
-function updateScreen(g, cont, fact, react, name)
- clearScreen(g)
- local row = 0
- local col = 0
+function displayFactories()
+ gpu:setBackground(0, 0.5, 1.0, 0.5)
+ gpu:setForeground(0, 0, 0, 1)
+ gpu:setText(0, row, string.format("%-64s", "Factories Output\\Input Status")) --65 chars (padded with spaces after)
+ gpu:setBackground(0,0,0,0)
+ gpu:setForeground(1,1,1,1)
 
- g:setBackground(0, 1.0, 0.5, 0.5)
- g:setForeground(0, 0, 0, 1)
- g:setText(col, row, string.format("%-59s", name .. " Monitoring")) --55 chars (padded with spaces after)
- row = row + 1
+ local factories = component.proxy(component.findComponent(siteNick))
+ if not factories then error("Factories was nil") end
+ print("Number of factories: " .. tableLength(factories))
 
- g:setBackground(0, 0.5, 1.0, 0.5)
- g:setForeground(0, 0, 0, 1)
- g:setText(col, row, string.format("%-54s", "Factories Output\\Input Status")) --55 chars (padded with spaces after)
- g:setBackground(0,0,0,0)
- g:setForeground(1,1,1,1)
- row = row + 1
- if (tableLength(fact) == 0) then g:setText(col + 1, row, "No factories") end
+ local invsSize = 0
+ for _, fctry in pairs(factories) do
+  local data_output = " OUT> "
+  local data_input = " IN> "
+  local invs
 
- local list = {}
- for n, m in pairs(fact) do
-  table.insert(list, n)
- end
- table.sort(list)
-
- -- factories
- for _, n in ipairs(list) do
-  local prod = "(" .. fact[n]["productivity"] .. "%) "
-  if (fact[n]["productivity"] < 50) then
-    g:setForeground(1,0,0,1)
-  elseif (fact[n]["productivity"] < 75) then
-    g:setForeground(1,1,0,1)
-  end
-  g:setText(col + 1, row, n .. prod .. fact[n]["outputs"])
-  row = row + 1
-  local indent = " "
-  while (indent:len() < (n:len() + prod:len())) do indent = indent .. " " end
-  g:setText(col + 1, row, indent .. fact[n]["inputs"])
-  g:setForeground(1,1,1,1)
-  row = row + 1
- end
- 
- row = row + 1
- g:setBackground(0, 0.5, 1.0, 0.5)
- g:setForeground(0, 0, 0, 1)
- g:setText(col, row, string.format("%-54s", "Reactor Status")) --55 chars (padded with spaces after)
- g:setBackground(0,0,0,0)
- g:setForeground(1,1,1,1)
- row = row + 1
-  
- --reactors
- for m, n in pairs(react) do
-  local prod = "(" .. n["productivity"] .. "%) "
-  if (n["productivity"] < 50) then
-    g:setForeground(1,0,0,1)
-  elseif (n["productivity"] < 75) then
-    g:setForeground(1,1,0,1)
-  end
-  g:setText(col + 1, row, m .. prod .. n["outputs"])
-  row = row + 1
-  local indent = " "
-  while (indent:len() < (m:len() + prod:len())) do indent = indent .. " " end
-  g:setText(col + 1, row, indent .. n["inputs"])
-  g:setForeground(1,1,1,1)
-  row = row + 1
- end
- 
- --containers
- row = row + 1
- g:setBackground(0, 0.5, 1.0, 0.5)
- g:setForeground(0, 0, 0, 1)
- g:setText(col, row, string.format("%-54s", "Container Inventory")) --55 chars (padded with spaces after)
- g:setBackground(0,0,0,0)
- g:setForeground(1,1,1,1)
- row = row + 1
- if (tableLength(cont) == 0) then g:setText(col + 1, row, "No containers") end
-
- list = {}
- for n, m in pairs(cont) do
-  table.insert(list, n)
- end
- table.sort(list)
- 
- for _, n in ipairs(list) do
-  if (row > 33) then
-   g:setText(col + 1, 33, "list truncated")
+  if (fctry:getType().Name == "Build_GeneratorNuclear_C") then
   else
-   g:setText(col + 1, row, string.format("%-20s", n) .. string.format("%3s", cont[n]) .. "%")
    row = row + 1
-  end
- end
+   local rec = fctry:getRecipe()
+   invs = fctry:getOutputInv()
+   invsSize = invs.Size - 1 -- Satisfactory reports back incorrect number HACK
+
+   -- summarizes output products
+   local rec_prod = rec:getProducts()
+   local prodTable = {}
+   for _, prodType in pairs(rec_prod) do
+    local n = createAbbrev(prodType.Type.Name)
+    prodTable[n] = "0%"
+   end
+
+   local i = 0
+   while (i < invsSize) do
+    local t = nil
+    local stack = invs:getStack(i)
+    if (stack.item) then t = stack.item.type end
+    if (t) then
+     local c = stack.count
+     local m = t.max
+     local n = createAbbrev(t.name)
+     prodTable[n] = makePercentage(c/m) .. "%"
+    end --if   
+    i = i + 1
+   end --while
+
+   for name, amnt in pairs(prodTable) do data_output = data_output .. name .. ":" .. amnt .. " " end
+
+   -- summarizes input ingredients
+   invs = fctry:getInputInv()
+   invsSize = invs.Size
+   local rec_ingred = rec:getIngredients()
+   local ingredTable = {}
+   for _, ingredType in pairs(rec_ingred) do
+    local n = createAbbrev(ingredType.Type.Name)
+    ingredTable[n] = "0%"
+   end
+   i = 0
+   while (i < invsSize) do
+    local t = nil
+    local stack = invs:getStack(i)
+    if (stack.item) then t = stack.item.type end
+    if (t) then
+     local c = stack.count
+     local m = t.max
+     local n = createAbbrev(t.name)
+     ingredTable[n] = makePercentage(c/m) .. "%"
+    end --if   
+    i = i + 1
+   end --while
+
+   for name, amnt in pairs(ingredTable) do data_input = data_input .. name .. ":" .. amnt .. " " end
+
+   -- outputs factory summaries to screen and network
+   local name = fctry.nick:sub(siteNick:len() + 2) --removes sitenick ie."ATOMICBAY "
+   local productivity = makePercentage(fctry.productivity)
+   if (fctry.productivity < .50) then
+    gpu:setForeground(1,0,0,1)
+   elseif (fctry.productivity < .75) then
+    gpu:setForeground(1,1,0,1)
+   end
+   gpu:setText(1, row, (name .. " (" .. productivity .. "%)" .. data_output))
+   gpu:setForeground(1,1,1,1)
+   row = row + 1
+   local indent = " "
+   while (indent:len() < (name:len() + tostring(productivity):len() + 4)) do indent = indent .. " " end
+   gpu:setText(1, row, (indent .. data_input))
+   net:broadcast(port, "factory", name, data_output, data_input, productivity)
+  end --if not nuclear plant
+ end --for
+end
+
+function displayReactors()
+ row = row + 1
+ gpu:setBackground(0, 0.5, 1.0, 0.5)
+ gpu:setForeground(0, 0, 0, 1)
+ gpu:setText(0, row, string.format("%-64s", "Reactor Output\\Input Status")) --65 chars (padded with spaces after)
+ gpu:setBackground(0,0,0,0)
+ gpu:setForeground(1,1,1,1)
+
+ local factories = component.proxy(component.findComponent(siteNick))
+ if not factories then error("Factories was nil") end
+ 
+ row = row + 1
+ local invsSize = 0
+ for _, fctry in pairs(factories) do
+  local data_output = " OUT> "
+  local data_input = " IN> "
+  local invs
+
+  if (fctry:getType().Name == "Build_GeneratorNuclear_C") then
+
+   local n = fctry:getInventories()[1] -- 1 output inventory (waste)
+   data_output = data_output .. "Nuclear Waste: " .. n.itemCount .. " "
+   n = fctry:getInventories()[2] -- 2 fuel inventory (water)
+   data_input = data_input .. "Water: " .. makePercentage(n.itemCount/50000) .. "% "
+   n = fctry:getInventories()[3] -- 3 inventory potential (rods)
+   data_input = data_input .. "Rods :" .. n.itemCount
   
- g:flush()
+   local name = fctry.nick:sub(siteNick:len() + 2) --removes site nickname plus space
+   local prod = makePercentage(fctry.productivity)
+   if (fctry.productivity < .50) then
+    gpu:setForeground(1,0,0,1)
+   elseif (fctry.productivity < .75) then
+    gpu:setForeground(1,1,0,1)
+   end
+   gpu:setText(1, row, (name .. " (" .. prod .. "%)" .. data_output))
+   row = row + 1
+   local indent = " "
+   while (indent:len() < (name:len() + tostring(prod):len() + 4)) do indent = indent .. " " end
+   gpu:setText(1, row, indent .. data_input)
+   gpu:setForeground(1,1,1,1)
+   row = row + 1
+   net:broadcast(port, "reactor", name, data_output, data_input, prod)
+
+  end --if nuclear plant
+ end --for
 end
 
 --main chunk
 local gpus = computer.getPCIDevices(classes.GPUT1)
-if tableLength(gpus) == 0 or tableLength(gpus) < 0 then error("GPU T1 count error!") end
-local screens = component.findComponent(classes.Build_Screen_C)
-if tableLength(screens) == 0 or tableLength(screens) < 4 then error("Screens count error!") end
-if not tableLength(screens) == tableLength(gpus) then error("Gpu to screens count mismatch!") end
+gpu = gpus[1]
+if not gpu then error("No GPU T1 found!") end
 
-gpu1 = gpus[1]
-gpu1:bindScreen(component.proxy(screens[1]))
-gpu1:setSize(60, 35)
-clearScreen(gpu1)
+local screen = component.proxy(component.findComponent(classes.Build_Screen_C)[1])
+if not screen then error("No screen") end
 
-
-gpu2 = gpus[2]
-gpu2:bindScreen(component.proxy(screens[2]))
-gpu2:setSize(60, 35)
-clearScreen(gpu2)
-
-gpu3 = gpus[3]
-gpu3:bindScreen(component.proxy(screens[3]))
-gpu3:setSize(60, 35)
-clearScreen(gpu3)
-
-gpu4 = gpus[4]
-gpu4:bindScreen(component.proxy(screens[4]))
-gpu4:setSize(60, 35)
-clearScreen(gpu4)
-
-local net = computer.getPCIDevices(classes.NetworkCard)[1]
+net = computer.getPCIDevices(classes.NetworkCard)[1]
 if not net then error("No network card") end
 
-event.ignoreAll()
-event.clear()
-event.listen(net)
-net:open(42)
-net:open(43)
-net:open(44)
-net:open(45)
-print("Opened ports")
+-- ATOMICBAY 42
+-- ATOMICAVE 43
+-- ATOMICWATERFALL 44
+-- ATOMICALCOVE 45
+port = 43
+siteNick = "ATOMICCAVE"
 
-containersPort42 = {}
-factoriesPort42 = {}
-reactorsPort42 = {}
-containersPort43 = {}
-factoriesPort43 = {}
-reactorsPort43 = {}
-containersPort44 = {}
-factoriesPort44 = {}
-reactorsPort44 = {}
-containersPort45 = {}
-factoriesPort45 = {}
-reactorsPort45 = {}
-lastUpdateTime = {}
+gpu:bindScreen(screen)
+gpu:setSize(65, 27)
 
 while true do
- local data = {event.pull()}
- e, receiver, sender, port, data = (function(e, receiver, sender, port, ...)
-  return e, receiver, sender, port, {...}
- end) (table.unpack(data))
-
- if e == "NetworkMessage" then
-  print("Updating data from port: " .. port)
-  if (port == 42) then
-   lastUpdateTime["Atomic Bay"] = computer.millis()
-   updateData(data, containersPort42, factoriesPort42, reactorsPort42)
-   updateScreen(gpu1, containersPort42, factoriesPort42, reactorsPort42, "Atomic Bay")
-  elseif (port == 43) then
-   lastUpdateTime["Atomic Cave"] = computer.millis()
-   updateData(data, containersPort43, factoriesPort43, reactorsPort43)
-   updateScreen(gpu2 , containersPort43, factoriesPort43, reactorsPort43, "Atomic Cave")
-  elseif (port == 44) then
-   lastUpdateTime["Atomic Waterfall"] = computer.millis()
-   updateData(data, containersPort44, factoriesPort44, reactorsPort44)
-   updateScreen(gpu3, containersPort44, factoriesPort44, reactorsPort44, "Atomic Waterfall")
-  elseif (port == 45) then
-   lastUpdateTime["Atomic Alcove"] = computer.millis()
-   updateData(data, containersPort45, factoriesPort45, reactorsPort45)
-   updateScreen(gpu4, containersPort45, factoriesPort45, reactorsPort45, "Atomic Alcove")
-  end 
-  if lastUpdateTime["Atomic Bay"] then
-   gpu1:setText(0, 34, "Last update: " .. computer.millis()-lastUpdateTime["Atomic Bay"] .. "ms")
-   gpu1:flush()
-  end
-  if lastUpdateTime["Atomic Cave"] then
-   gpu2:setText(0, 34, "Last update: " .. computer.millis()-lastUpdateTime["Atomic Cave"] .. "ms")
-   gpu2:flush()
-  end
-  if lastUpdateTime["Atomic Waterfall"] then
-   gpu3:setText(0, 34, "Last update: " .. computer.millis()-lastUpdateTime["Atomic Waterfall"] .. "ms")
-   gpu3:flush()
-  end
-  if lastUpdateTime["Atomic Alcove"] then
-   gpu4:setText(0, 34, "Last update: " .. computer.millis()-lastUpdateTime["Atomic Alcove"] .. "ms")
-   gpu4:flush()
-  end
+ row = 0
+ clearScreen(gpu)
+ displayFactories()
+ displayReactors()
+ displayContainers()
+ local _, h = gpu:getSize()
+ if (row > h) then
+  gpu:setForeground(1,0,0,1)
+  row = h - 2
  end
+ gpu:setText(0, row, "Run time: " .. convertToTime(computer.millis()))
+ gpu:setForeground(1,1,1,1)
+ gpu:flush()
+ event.pull(5)
 end
