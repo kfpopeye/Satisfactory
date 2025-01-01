b2d990abb26c9b16e2cd32525b44b2903cb4d335
kfpopeye <kfpopeye@users.noreply.github.com>
2024-12-31 20:49:10 -0800

    Update projectProduction.lua
    v1.1 - now reports any factories connected to network

--- Lua/projectProduction.lua
+++ Lua/projectProduction.lua
@@ -3,7 +3,7 @@
 -- |   projectProduction.lua                                    |
 -- |                                                           |
 -- -------------------------------------------------------------
-computer.log(1, "--- Project Production v1.0 ---")
+computer.log(1, "--- Project Production v1.1 ---")
 
 function tableLength(T)
   local count = 0
@@ -28,7 +28,7 @@ function makePercentage(x)
  return num
 end
 
-function displayManufacturer(m)
+function displayManufacturer(m, index, total)
  clearScreen(gpu)
  print(m.internalName)
  gpu:setBackground(0, 0.5, 1.0, 0.5)
@@ -37,7 +37,7 @@ function displayManufacturer(m)
  gpu:setBackground(0,0,0,0)
  gpu:setForeground(1,1,1,1)
 
- gpu:setText(0, 1, "Device: " .. m:getType().displayName)
+ gpu:setText(0, 1, "Device: " .. m:getType().displayName .. " (" .. index .. "/" .. total .. ")")
  gpu:setText(0, 2, "Recipe: " .. m:getRecipe().Name)
  local ingredients = m:getRecipe():getIngredients()
  local prod = makePercentage(m.Productivity)
@@ -78,22 +78,14 @@ end
 
 function loop()
  while true do
-  -- assembler
-  local device = component.proxy("5198135C41F35DC42D08C6B1BDF29E69")
-  if device then displayManufacturer(device) else print("Machine #1 not found") end
-  event.pull(5.0)
-  -- particle accelerator
-  device = component.proxy("01564603469D22B479BDD8B89332F453")
-  if device then displayManufacturer(device) else print("Machine #2 not found") end
-  event.pull(5.0)
-  -- manufacturer #1
-  device = component.proxy("3D1D25B64C1E87F9C527FAADDC4FEAB6")
-  if device then displayManufacturer(device) else print("Machine #3 not found") end
-  event.pull(5.0)
-  -- assembler #2
-  device = component.proxy("A257C42E40709C9B9B3C21A6317EC9A0")
-  if device then displayManufacturer(device) else print("Machine #4 not found") end
-  event.pull(5.0)
+  local manufacturers = component.findComponent(classes.Manufacturer)
+  numManufacturers = tableLength(manufacturers)
+  print ("Found manufacturers: " .. numManufacturers)
+  for i, m in ipairs(manufacturers) do
+   local device = component.proxy(m)
+   if device then displayManufacturer(device, i, numManufacturers) else print("Device not found") end
+   event.pull(5.0)
+  end
  end
 end
 
@@ -102,7 +94,7 @@ local gpus = computer.getPCIDevices(classes.GPUT1)
 gpu = gpus[1]
 if not gpu then error("No GPU T1 found!") end
 
-local screen = component.proxy("D47622D74E376FBC9DE80FB37F192134")
+local screen = component.proxy(component.findComponent(classes.Build_Screen_C)[1])
 if not screen then error("No screen") end
 
 gpu:bindScreen(screen)
